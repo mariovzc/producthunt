1b584815d111a431e3393e2a789cd1a02f4a75c3
¨Mario <mario.vizcainos@gmail.com>
2017-10-18 11:03:07 -0500

    give acces to the pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :private_access, except: [:index, :show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all    
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def signed_in?
-    !current_user.nil?
-  end
-  
-  def current_user
-    @current_user ||= User.find(cookies.signed[:user_id]) if cookies.signed[:user_id]
-  rescue ActiveRecord::RecordNotFound
-  end
 end
